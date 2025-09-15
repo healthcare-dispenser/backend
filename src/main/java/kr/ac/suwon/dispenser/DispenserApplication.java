43e536ac88b72c85b1644d45d6737a7dec159dc6
sehyeon <Eugene8500@gmail.com>
2025-09-15 10:07:17 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/kr/ac/suwon/dispenser/DispenserApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.suwon.dispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/suwon/dispenser/DispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.suwon.dispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
