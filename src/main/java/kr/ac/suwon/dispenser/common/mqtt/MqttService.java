a99181531d15465d3fe7b08f9337f43c99c9d75c
sehyeon <Eugene8500@gmail.com>
2025-09-23 09:37:28 +0900

    fix: MQTT 퍼블리쉬 오류 해결

--- src/main/java/kr/ac/suwon/dispenser/common/mqtt/MqttService.java
+++ src/main/java/kr/ac/suwon/dispenser/common/mqtt/MqttService.java
@@ -4,7 +4,11 @@ import kr.ac.suwon.dispenser.common.JsonMapper;
 import kr.ac.suwon.dispenser.dispenser.dto.DispenserRegisterResponse;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +20,15 @@ public class MqttService {
 
     public void publishJson(String topic, Object body) {
         String json = mapper.toJson(body);
+        MqttMessage msg = new MqttMessage(json.getBytes(StandardCharsets.UTF_8));
+        msg.setQos(1);
+        msg.setRetained(false);
 
+        try {
+            mqttClient.publish(topic, msg);
+        } catch (MqttException e) {
+            throw new RuntimeException("MQTT 퍼블리쉬 오류");
+        }
     }
 
     public void publishRegisterResponse(String uuid, DispenserRegisterResponse response) {
