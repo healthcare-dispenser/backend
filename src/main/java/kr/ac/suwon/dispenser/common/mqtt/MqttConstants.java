cea28554872caf73595a0e1d99e0d8d87c824ba5
sehyeon <Eugene8500@gmail.com>
2025-09-23 09:50:50 +0900

    fix: MQTT 토픽 경로 오류 해결

--- src/main/java/kr/ac/suwon/dispenser/common/mqtt/MqttConstants.java
+++ src/main/java/kr/ac/suwon/dispenser/common/mqtt/MqttConstants.java
@@ -4,6 +4,6 @@ public abstract class MqttConstants {
     public static final String DISPENSER_REGISTER_ALL = "dispenser/+/register";
 
     public static String getRegisterResponseTopic(String uuid) {
-        return "dispenser/" + uuid + "register/response";
+        return "dispenser/" + uuid + "/register/response";
     }
 }
