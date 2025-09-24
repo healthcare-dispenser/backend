0d64a2888ad1565265b7563c592813c31f03d320
sehyeon <Eugene8500@gmail.com>
2025-09-24 22:05:12 +0900

    chore: DB 초기값 추가

--- src/main/java/kr/ac/suwon/dispenser/DbInit.java
+++ src/main/java/kr/ac/suwon/dispenser/DbInit.java
@@ -10,6 +10,7 @@ import kr.ac.suwon.dispenser.profile.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,26 +21,31 @@ public class DbInit {
     private final AccountRepository accountRepository;
     private final DispenserRepository dispenserRepository;
     private final ProfileRepository profileRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void init() {
-        Account account = Account.create("a@a.com", "123");
-        accountRepository.save(account);
+        Account a1 = Account.create("a@a.com", passwordEncoder.encode("123"));
+        Account a2 = Account.create("test", passwordEncoder.encode("test"));
 
-        Profile p1 = Profile.create(account, "세현", 180.0, 70.0, Gender.MALE);
-        Profile p2 = Profile.create(account, "다인", 160.0, 40.0, Gender.FEMALE);
-        Profile p3 = Profile.create(account, "다혜", 160.0, 40.0, Gender.FEMALE);
+        accountRepository.save(a1);
+        accountRepository.save(a2);
+
+        Profile p1 = Profile.create(a1, "세현", 180.0, 70.0, Gender.MALE);
+        Profile p2 = Profile.create(a1, "다인", 160.0, 40.0, Gender.FEMALE);
+        Profile p3 = Profile.create(a1, "다혜", 160.0, 40.0, Gender.FEMALE);
+        Profile p4 = Profile.create(a2, "test", 160.0, 40.0, Gender.MALE);
 
         profileRepository.save(p1);
         profileRepository.save(p2);
         profileRepository.save(p3);
+        profileRepository.save(p4);
 
-        Dispenser dispenser = Dispenser.create("TEST_UUID");
-        dispenser.assignAccount(account);
+        Dispenser dispenser = Dispenser.create("TEST-UUID");
+        dispenser.assignAccount(a1);
+
         dispenserRepository.save(dispenser);
-
-
     }
 }
